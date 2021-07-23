58856f214f212c2f43abbfda1d5f6c6efe8a5e3e
Navoda Perera <navoda1@gmail.com>
2021-07-22 22:18:11 -0400

    added web server sources data files and updated the main.cpp to start the server. Tested by updating a text field in the web page with accelerometer data

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <ArduinoJson.h>
 
+#include "WebServer.h"
 #include "MPU6050.h"
 
 // global variables
+WebServer *web_server;
 MPU6050 *mpu_6050;
 int16_t accel_reading[3];
+char json_buffer[30];
 
 void setup() {
     Serial.begin (115200);
+    web_server = new WebServer(80);
     mpu_6050 = new MPU6050(0x68);
 }
 
@@ -20,6 +25,14 @@ void loop()
     Serial.print(accel_reading[0]); Serial.print(" | ");
     Serial.print(accel_reading[1]); Serial.print(" | ");
     Serial.println(accel_reading[2]);
+
+    // convert readings to json string
+    StaticJsonDocument<48> doc;
+    copyArray(accel_reading, doc.to<JsonArray>());
+    serializeJson(doc, json_buffer);
+
+    // send the event to the browser
+    web_server->send_event("accel_readings", json_buffer);
 
     delay (200);
 }
